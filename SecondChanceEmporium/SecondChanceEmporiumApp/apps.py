95f8a8074c543c700865223cefee66b336b7dd46
gorgilazarev3 <gorgi.lazarev3@gmail.com>
2023-07-27 11:27:22 +0200

    Initial project skeleton established

---
+++ SecondChanceEmporium/SecondChanceEmporiumApp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SecondchanceemporiumappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'SecondChanceEmporiumApp'
