43cb04a5b9f28a49244b5a19b44d0f7ba2a1c250
gorgilazarev3 <gorgi.lazarev3@gmail.com>
2023-07-27 23:51:16 +0200

    Finished the design for the home page

--- SecondChanceEmporium/SecondChanceEmporium/urls.py
+++ SecondChanceEmporium/SecondChanceEmporium/urls.py
@@ -18,7 +18,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from SecondChanceEmporiumApp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',index,name="index")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- SecondChanceEmporium/SecondChanceEmporiumApp/views.py
+++ SecondChanceEmporium/SecondChanceEmporiumApp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+def index(request):
+    newest_products = Product.objects.all().reverse()[:4]
+    context = {"newest_products":newest_products}
+    return render(request,"index.html",context=context)
