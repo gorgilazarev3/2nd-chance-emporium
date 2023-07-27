82066fba9f364de74b6717e0ae98f82ee6d87b8f
gorgilazarev3 <gorgi.lazarev3@gmail.com>
2023-07-27 17:20:16 +0200

    Added initial models

--- SecondChanceEmporium/SecondChanceEmporiumApp/admin.py
+++ SecondChanceEmporium/SecondChanceEmporiumApp/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return True
+
+    def has_delete_permission(self, request, obj=None):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        return True
+
+class ShopUserAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        if obj:
+            return request.user == obj.user_id
+
+
+class ProductAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        if request.user:
+            return True
+
+    def has_delete_permission(self, request, obj=None):
+        if obj and request.user == obj.seller.user_id and obj.seller.is_seller:
+            return True
+
+    def has_change_permission(self, request, obj=None):
+        if obj and request.user == obj.seller.user_id and obj.seller.is_seller:
+            return True
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(ShopUser, ShopUserAdmin)
+admin.site.register(Product, ProductAdmin)
--- SecondChanceEmporium/SecondChanceEmporiumApp/models.py
+++ SecondChanceEmporium/SecondChanceEmporiumApp/models.py
@@ -1,3 +1,42 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class ShopUser(models.Model):
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE,blank=True,null=True)
+    name = models.CharField(max_length=255,blank=False)
+    email = models.EmailField()
+    is_seller = models.BooleanField()
+    rating = models.DecimalField(decimal_places=1,max_digits=2,default=0.0,blank=True)
+
+    def __str__(self):
+        return f"{self.user_id.username}"
+
+class Category(models.Model):
+    category = models.CharField(max_length=255,blank=False,null=False)
+
+    def __str__(self):
+        return f"{self.category}"
+
+
+class ProductImage(models.Model):
+    image = models.ImageField(upload_to="files/")
+
+class Product(models.Model):
+    title = models.CharField(max_length=255,blank=False,null=False)
+    category = models.ForeignKey(to=Category,on_delete=models.CASCADE)
+    description = models.CharField(max_length=255,blank=False,null=False)
+    images = models.ManyToManyField(to=ProductImage,symmetrical=False,blank=True)
+    cover_image = models.ImageField(upload_to="files/")
+    is_approved = models.BooleanField()
+    seller = models.ForeignKey(to=ShopUser,on_delete=models.CASCADE)
+    price = models.DecimalField(decimal_places=2,max_digits=10)
+
+    def __str__(self):
+        return f"{self.title} - {self.category} - {self.price}$"
+
+class Order(models.Model):
+    product = models.ForeignKey(to=Product,on_delete=models.CASCADE)
+    buyer = models.ForeignKey(to=ShopUser,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.product.title} - {self.buyer.name} - {self.product.price}$"
